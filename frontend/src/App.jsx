69a6360b1c7a57d23de0df4dd69b78ebfd46c292
devansh190804 <devanshpandit2004@gmail.com>
2025-07-06 13:46:25 +0530

    Debuging frontend build issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import DashboardLayout from './layout/DashboardLayout';
 import Login from './pages/Login';
 import OpenRoute from './components/auth/OpenRoute';
 import PrivateRoute from './components/auth/PrivateRoute';
-import Signup from './pages/signup';
+// import Signup from './pages/Signup';
 import Tasks from './pages/Tasks';
 
 const App = () => {
@@ -16,7 +16,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<OpenRoute><Login /></OpenRoute>} />
-        <Route path="/signup" element={<OpenRoute><Signup /></OpenRoute>} />
+        {/* <Route path="/signup" element={<OpenRoute><Signup /></OpenRoute>} />  */}
         <Route path="/dashboard/*" element={<PrivateRoute><DashboardLayout /></PrivateRoute>}>
           <Route index element={<Dashboard />} />
           <Route path="project/:projectId" element={<Tasks />} />
